d208976cf2409b780acc003f4be45166ca299b27
Moe Myint Zaw <moemyintzaw17@gmail.com>
2024-04-28 15:48:39 +0630

    building-static-or-hardcoding-blogs-system

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blogs System</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+   <article>
+    <h1>First Blog</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, illo, deleniti dolorem nesciunt facilis, 
+        ipsum quidem deserunt totam nostrum facere repellat non. Error, modi eos rerum alias voluptatum autem veritatis!</p>
+   </article>
+   <a href="/">go back</a>
+</body>
+</html>
---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blogs System</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+   <article>
+    <h1><a href="/blog">First Blog</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, illo, deleniti dolorem nesciunt facilis, 
+        ipsum quidem deserunt totam nostrum facere repellat non. Error, modi eos rerum alias voluptatum autem veritatis!</p>
+   </article>
+   <h1><a href="/blog">Second Blog</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, illo, deleniti dolorem nesciunt facilis, 
+        ipsum quidem deserunt totam nostrum facere repellat non. Error, modi eos rerum alias voluptatum autem veritatis!</p>
+   </article>
+   <h1><a href="/blog">Third Blog</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, illo, deleniti dolorem nesciunt facilis, 
+        ipsum quidem deserunt totam nostrum facere repellat non. Error, modi eos rerum alias voluptatum autem veritatis!</p>
+   </article>
+</body>
+</html>
