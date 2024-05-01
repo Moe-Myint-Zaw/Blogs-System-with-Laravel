00af3a3d0cadf5c1e8e87facc1bce84679b9cc52
Moe Myint Zaw <moemyintzaw17@gmail.com>
2024-05-01 11:59:40 +0630

    Refactor-&laravel-path-helper-function

---
+++ app/Models/Blog.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+class Blog
+{
+    public static function find($slug){
+        $path=resource_path("blogs/$slug.html");
+   if(!file_exists($path)){
+    return redirect('/');
+   }
+    return $blog=cache()->remember("posts.$slug",120,function()use ($path){
+        return file_get_contents($path);
+    });
+    }
+}
+
+
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Blog;
 
 Route::get('/', function () {
     return view('blogs');
 });
 
 Route::get('/blogs/{blog}', function ($slug) {
-    $path=__DIR__."/../resources/blogs/$slug.html";
-   if(!file_exists($path)){
-    return redirect('/');
-   }
-    $blog=cache()->remember("posts.$slug",120,function()use ($path){
-        var_dump('hello');
-        return file_get_contents($path);
-    });
+    
     return view('blog',[
-        'blog'=>$blog
+        'blog'=>Blog::find($slug)
     ]);
 })->where('blog','[A-z\d\-_]+');
 
