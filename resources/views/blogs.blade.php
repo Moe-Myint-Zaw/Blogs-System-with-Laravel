9a322bc78447953f72205b98f68987b35a60d935
Moe Myint Zaw <moemyintzaw17@gmail.com>
2024-05-01 13:16:41 +0630

    fetch-all-blogs(use File Facades)

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -7,20 +7,8 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-   <article>
-    <h1><a href="/blogs/first-blog">First Blog</a></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, illo, deleniti dolorem nesciunt facilis, 
-        ipsum quidem deserunt totam nostrum facere repellat non. Error, modi eos rerum alias voluptatum autem veritatis!</p>
-   </article>
-   <article>
-   <h1><a href="/blogs/second-blog">Second Blog</a></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, illo, deleniti dolorem nesciunt facilis, 
-        ipsum quidem deserunt totam nostrum facere repellat non. Error, modi eos rerum alias voluptatum autem veritatis!</p>
-   </article>
-   <article>
-   <h1><a href="/blogs/third-blog">Third Blog</a></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, illo, deleniti dolorem nesciunt facilis, 
-        ipsum quidem deserunt totam nostrum facere repellat non. Error, modi eos rerum alias voluptatum autem veritatis!</p>
-   </article>
+  <?php foreach($blogs as $blog): ?>
+    <?= $blog ?>
+    <?php endforeach; ?>
 </body>
 </html>
