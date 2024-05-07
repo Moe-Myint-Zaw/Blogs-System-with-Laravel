e9aae2c08bd0b8b9a1c5dd3edc9842d50fb03b95
Moe Myint Zaw <moemyintzaw17@gmail.com>
2024-05-07 22:53:09 +0630

    Section-&-Yield

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blogs System</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
- <h1>{{$blog->title}}</h1>
+@extends('layout')
+@section('content')
+<h1>{{$blog->title}}</h1>
  <p>Published at : {{$blog->date}}</p>
  <p>{!!$blog->body!!}</p>
    <a href="/">go back</a>
-</body>
-</html>
+@endsection
+ 
+ 
+ 
+ 
+ 
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blogs System</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<style>
-    .bg-blue{
-        background-color:lightblue;
-    }
-</style>
-<body>
-    @if(true)
+@extends('layout')
+@section('content')
+@if(true)
         This is conditional content.
     
     @endif
@@ -25,5 +13,8 @@
     </div>
   </div>
     @endforeach;
-</body>
-</html>
+@endsection
+    
+    
+    
+   
