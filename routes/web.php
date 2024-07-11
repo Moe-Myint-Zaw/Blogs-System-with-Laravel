abbb8e52a63260bf6c33baa5d1f457f71ce91872
Moe Myint Zaw <moemyintzaw17@gmail.com>
2024-07-11 21:02:41 +0630

    filter-by-author

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,7 @@ use App\Models\Category;
 Route::get('/',[BlogController::class,'index']);
 
 Route::get('/blogs/{blog:slug}',[BlogController::class,'show'])->where('blog','[A-z\d\-_]+');
-Route::get('/users/{user:username}', function (User $user) {
-    
-     return view('blogs',[
-        'blogs'=>$user->blogs,
-        'categories'=>Category::all(),
-    ]);
-});
+
 
     
 
