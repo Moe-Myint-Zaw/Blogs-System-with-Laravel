0cf64118531b706b1e41449e5de8b98e44c0b142
Moe Myint Zaw <136053426+Moe-Myint-Zaw@users.noreply.github.com>
2024-07-11 13:03:56 +0630

    laravel_advance-query-scope

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,7 +10,7 @@ class BlogController extends Controller
     public function index(){
         
     return view('blogs',[
-        'blogs'=>$this->getBlogs(),
+        'blogs'=>Blog::latest()->filter(request(['search']))->get(),
         'categories'=>Category::all()
     ]);
     }
@@ -20,14 +20,5 @@ class BlogController extends Controller
             'blog'=>$blog,
             'randomBlogs'=>Blog::inRandomOrder()->take(3)->get()
         ]);
-    }
-
-    protected function getBlogs(){
-        $blogs=Blog::latest();
-        if(request('search')){
-        $blogs=$blogs->where('title','LIKE','%'.request('search').'%')
-                    ->orWhere('body','LIKE','%'.request('search').'%');
-    }
-        return $blogs->get();
     }
 }
